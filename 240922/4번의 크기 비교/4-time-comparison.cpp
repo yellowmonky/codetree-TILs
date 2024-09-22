04b73b0acc653a18e7b8343083990cc6f738befe
yellowmonky <132804858+yellowmonky@users.noreply.github.com>
2024-09-22 16:00:32 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240922/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d,e;
+
+    cin>>a>>b>>c>>d>>e;
+
+    if(a>b){
+        cout<<"1"<<endl;
+    }
+    else{
+        cout<<"0"<<endl;
+    }
+
+    if(a>c){
+        cout<<"1"<<endl;
+    }
+    else{
+        cout<<"0"<<endl;
+    }
+
+    if(a>d){
+        cout<<"1"<<endl;
+    }
+    else{
+        cout<<"0"<<endl;
+    }
+
+    if(a>e){
+        cout<<"1"<<endl;
+    }
+    else{
+        cout<<"0"<<endl;
+    }
+    return 0;
+}
