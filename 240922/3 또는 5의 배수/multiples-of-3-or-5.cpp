83d94ad6d16f3db45f70fcad9d561758f4653749
yellowmonky <132804858+yellowmonky@users.noreply.github.com>
2024-09-22 15:56:37 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240922/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    if(n%3==0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+
+    if(n%5==0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    
+    return 0;
+}
