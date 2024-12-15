7817a47d806915456ad5bc6ff914bc90ed86bed5
yellowmonky <132804858+yellowmonky@users.noreply.github.com>
2024-12-16 01:51:24 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241216/두 사람/two-person.cpp
+++ 241216/두 사람/two-person.cpp
@@ -2,21 +2,19 @@
 using namespace std;
 
 int main() {
-    
-struct {
-    int age;
-    char sex;
-} person; // 여기에 세미콜론 추가
+    struct person{
+        int age;
+        char sex;
+    }; // 세미콜론 추가
 
-    person person1,person2;
+    person person1, person2;
 
-    cin>>person1.age>>person1.sex;
-    cin>>person2.age>>person2.sex;
+    cin >> person1.age >> person1.sex;
+    cin >> person2.age >> person2.sex;
 
-    if(person1.sex=='M'||person2.sex=='M'){
-        cout<<1;
+    if (person1.sex == 'M' || person2.sex == 'M') {
+        cout << 1;
     }
-
 
     return 0;
 }
