6482776d6e1378e85752e4ee8f7b7610f08bd296
yellowmonky <132804858+yellowmonky@users.noreply.github.com>
2024-12-16 20:46:47 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241216/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int c = 0;
+
+    cin >> n;
+
+    while(c < n){
+        cout<<'*'<<endl;
+        c ++;
+    }
+    return 0;
+}
