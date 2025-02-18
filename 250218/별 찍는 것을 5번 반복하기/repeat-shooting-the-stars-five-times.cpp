09996fe9ded0dbc114d39b057121754f0e3f4f55
yellowmonky <132804858+yellowmonky@users.noreply.github.com>
2025-02-18 11:00:26 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250218/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void onelinestar(){
+    cout<<"**********"<<endl;
+}
+
+int main() {
+    // Write your code here!
+    for(int i = 0; i < 5; i++){
+        onelinestar();
+    }
+
+    return 0;
+}
